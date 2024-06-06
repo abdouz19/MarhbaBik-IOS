b90ad2017c3c34014a9475ef40b8f7b667d9f2e0
Abderraouf ZOUAID <mohamedabderraouf.zouaid.lp1@gmail.com>
2024-06-06 19:29:32 +0100

    17th commit

--- lib/widgets/wilaya_item.dart
+++ lib/widgets/wilaya_item.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:marhba_bik/models/wilaya.dart';
+import 'package:marhba_bik/screens/traveler/wilaya_screen.dart';
 
 class WilayaItem extends StatelessWidget {
   final Wilaya wilaya;
@@ -9,52 +10,60 @@ class WilayaItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 7),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: CachedNetworkImage(
-              imageUrl: wilaya.imageUrl,
-              width: 160,
-              height: 120,
-              fit: BoxFit.cover,
-              placeholder: (context, url) {
-                return const Center(child: CircularProgressIndicator());
-              },
-              errorWidget: (context, url, error) => const Icon(Icons.error),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => WilayaScreen(wilaya: wilaya)),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 7),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: CachedNetworkImage(
+                imageUrl: wilaya.imageUrl,
+                width: 160,
+                height: 120,
+                fit: BoxFit.cover,
+                placeholder: (context, url) {
+                  return const Center(child: CircularProgressIndicator());
+                },
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
             ),
-          ),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 3),
-              Text(
-                wilaya.name,
-                style: const TextStyle(
-                  color: Color(0xff001939),
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'KastelovAxiforma',
-                  fontSize: 18,
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 3),
+                Text(
+                  wilaya.name,
+                  style: const TextStyle(
+                    color: Color(0xff001939),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'KastelovAxiforma',
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                wilaya.description,
-                style: const TextStyle(
-                  color: Color(0xff001939),
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'KastelovAxiforma',
-                  fontSize: 14,
+                const SizedBox(height: 5),
+                Text(
+                  wilaya.description,
+                  style: const TextStyle(
+                    color: Color(0xff001939),
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'KastelovAxiforma',
+                    fontSize: 14,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
